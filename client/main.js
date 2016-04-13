f566121abc78dfc5a7b6af7d94b918193b5d98b9
anuragnagarkota <anuragnagarkota@gmail.com>
2016-04-13 14:37:23 +0530

    main.js changed and the bug has been fixed

--- client/main.js
+++ client/main.js
@@ -17,6 +17,7 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
+	// i have changed it when i pulled it from remote git.
     instance.counter.set(instance.counter.get() + 1);
   },
 });
